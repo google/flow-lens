65f7a4dda08d2952973b7b9cff4216c2e5e4139d
Mitchell spano <MitchSpano@gmail.com>
2024-12-23 22:20:03 -0600

    Fix XML reader tests

--- src/xml_reader_test.ts
+++ src/xml_reader_test.ts
@@ -1,10 +1,8 @@
-import { it, expect, describe, beforeEach, afterEach } from "jasmine";
-import * as fs from "node:fs";
-import * as path from "node:path";
+import { assertEquals, assertThrows } from "@std/assert";
+import * as path from "@std/path";
 import { ERROR_MESSAGES, XmlReader } from "./xml_reader.ts";
 
-const { TEST_UNDECLARED_OUTPUTS_DIR: TEST_UNDECLARED_OUTPUTS_DIR } =
-  process.env as { [index: string]: string };
+const TEST_UNDECLARED_OUTPUTS_DIR = "./";
 
 const VALID_FILE_PATH = path.join(TEST_UNDECLARED_OUTPUTS_DIR, "test.xml");
 const INVALID_FILE_PATH = "nonexistent.xml";
@@ -14,70 +12,64 @@ const INVALID_FILE_EXTENSION_PATH = path.join(
 );
 const XML_CONTENT = "<root></root>";
 
-describe("XmlReader", () => {
-  let caught: Error | undefined;
-  let xmlReader: XmlReader;
-
-  beforeEach(() => {
-    fs.writeFileSync(VALID_FILE_PATH, XML_CONTENT);
-  });
-
-  afterEach(() => {
-    fs.unlinkSync(VALID_FILE_PATH);
-  });
-
-  describe("getXmlFileBody", () => {
-    it("should read the XML file content when the file path is valid", () => {
-      xmlReader = new XmlReader(VALID_FILE_PATH);
-
-      const xmlContent = xmlReader.getXmlFileBody();
-
-      expect(xmlContent).toBe(xmlContent);
-    });
-
-    it("should read the XML regardless of the case of the file extension", () => {
-      const xmlFilePath = path.join(TEST_UNDECLARED_OUTPUTS_DIR, "test.XmL");
-      fs.writeFileSync(xmlFilePath, XML_CONTENT);
-      xmlReader = new XmlReader(xmlFilePath);
-
-      const xmlContent = xmlReader.getXmlFileBody();
-
-      expect(xmlContent).toBe(xmlContent);
-
-      fs.unlinkSync(xmlFilePath);
-    });
-
-    it("should throw an error when the file path does not exist", () => {
-      xmlReader = new XmlReader(INVALID_FILE_PATH);
-
-      try {
-        xmlReader.getXmlFileBody();
-      } catch (error: unknown) {
-        caught = error as Error;
+Deno.test("XmlReader", async (t) => {
+  await t.step("getXmlFileBody", async (t) => {
+    await t.step(
+      "should read the XML file content when the file path is valid",
+      async () => {
+        await Deno.writeFile(
+          VALID_FILE_PATH,
+          new TextEncoder().encode(XML_CONTENT)
+        );
+        const xmlReader = new XmlReader(VALID_FILE_PATH);
+        const xmlContent = xmlReader.getXmlFileBody();
+        assertEquals(xmlContent, XML_CONTENT);
+        await Deno.remove(VALID_FILE_PATH);
       }
+    );
 
-      expect(caught).toBeDefined();
-      expect(caught?.message).toEqual(
-        ERROR_MESSAGES.invalidFilePath(INVALID_FILE_PATH)
-      );
-    });
-
-    it("should throw an error when the file extension is not XML", () => {
-      fs.writeFileSync(INVALID_FILE_EXTENSION_PATH, XML_CONTENT);
-      xmlReader = new XmlReader(INVALID_FILE_EXTENSION_PATH);
-
-      try {
-        xmlReader.getXmlFileBody();
-      } catch (error: unknown) {
-        caught = error as Error;
+    await t.step(
+      "should read the XML regardless of the case of the file extension",
+      async () => {
+        const xmlFilePath = path.join(TEST_UNDECLARED_OUTPUTS_DIR, "test.XmL");
+        await Deno.writeFile(
+          xmlFilePath,
+          new TextEncoder().encode(XML_CONTENT)
+        );
+        const xmlReader = new XmlReader(xmlFilePath);
+        const xmlContent = xmlReader.getXmlFileBody();
+        assertEquals(xmlContent, XML_CONTENT);
+        await Deno.remove(xmlFilePath);
       }
+    );
 
-      expect(caught).toBeDefined();
-      expect(caught?.message).toEqual(
-        ERROR_MESSAGES.invalidFileExtension(INVALID_FILE_EXTENSION_PATH)
-      );
+    await t.step(
+      "should throw an error when the file path does not exist",
+      async () => {
+        const xmlReader = new XmlReader(INVALID_FILE_PATH);
+        assertThrows(
+          () => xmlReader.getXmlFileBody(),
+          Error,
+          ERROR_MESSAGES.invalidFilePath(INVALID_FILE_PATH)
+        );
+      }
+    );
 
-      fs.unlinkSync(INVALID_FILE_EXTENSION_PATH);
-    });
+    await t.step(
+      "should throw an error when the file extension is not XML",
+      async () => {
+        await Deno.writeFile(
+          INVALID_FILE_EXTENSION_PATH,
+          new TextEncoder().encode(XML_CONTENT)
+        );
+        const xmlReader = new XmlReader(INVALID_FILE_EXTENSION_PATH);
+        assertThrows(
+          () => xmlReader.getXmlFileBody(),
+          Error,
+          ERROR_MESSAGES.invalidFileExtension(INVALID_FILE_EXTENSION_PATH)
+        );
+        await Deno.remove(INVALID_FILE_EXTENSION_PATH);
+      }
+    );
   });
 });
